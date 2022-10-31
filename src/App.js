0d6f966313189fa2b572e0f5cf52d0cd0fa014cd
Didenchuk <tdidenchuk@gmail.com>
2022-10-31 17:30:49 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
-import logo from"./logo.png";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"  />
-        
-      </header>
+        </header>
+        <main>
+          <Dictionary/>
+        </main>
+        <footer className="App-footer">
+         <small>Coded by Tanya Didenchuk</small> 
+        </footer>
+        </div>
     </div>
   );
 }
