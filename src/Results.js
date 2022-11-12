71ac118b634db0a2305fb6878f870ab620e15edb
Didenchuk <tdidenchuk@gmail.com>
2022-11-12 19:52:31 +0100

    Styled the application

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 
 export default function Results(props) {
     if (props.results) {
         return(
-        <div className="Results">
+            <div className="Results">
+                <section>
                 <h2>
                     {props.results.word}
                 </h2>
@@ -17,10 +19,11 @@ export default function Results(props) {
                             </div>
                     )
                 })}
+                    </section>
                 {props.results.meanings.map(function (meaning, index) {
-                    return (<div key={index}>
+                    return (<section key={index}>
                   <Meaning meaning={meaning}/>
-                    </div>);
+                    </section>);
                 })}
         </div>)
     } else {
